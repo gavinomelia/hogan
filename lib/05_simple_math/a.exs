e087758df850fef90775bb17b94eed6495e1200a
Gavin OMelia <gavin@omelia.org>
2017-08-10 17:33:43 -0400

    Improve simple math a

---
+++ lib/05_simple_math/a.exs
@@ -0,0 +1,21 @@
+defmodule SimpleMath do
+  def num(:one), do: IO.gets("What is the first number? ") |> String.strip |> Float.parse
+  def num(:two), do: IO.gets("What is the second number? ") |> String.strip |> Float.parse
+  
+  def go do
+    {one, _} = num(:one)
+    {two, _} = num(:two)
+    IO.puts """
+    #{one} + #{two} = #{math("+", one, two)}
+    #{one} - #{two} = #{math("-", one, two)}
+    #{one} * #{two} = #{math("*", one, two)}
+    #{one} / #{two} = #{math("/", one, two)}
+    """
+  end
+
+  def math("+", one, two), do: one + two
+  def math("-", one, two), do: one - two
+  def math("*", one, two), do: one * two
+  def math("/", one, two), do: one / two
+end
+SimpleMath.go
